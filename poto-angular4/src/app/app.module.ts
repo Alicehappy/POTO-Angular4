083c57973ff8acaec8e7264bdbdcf10e2385e10c
Alice <alicezou05@gmail.com>
2017-11-10 22:28:39 -0500

    Tutorial - Routing: Add the CSS style for HeroDetailComponent in the hero-detail.component.css file.

--- poto-angular4/src/app/app.module.ts
+++ poto-angular4/src/app/app.module.ts
@@ -3,21 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
-import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
+    HeroesComponent,    
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
